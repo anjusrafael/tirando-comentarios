d2f6d04ff479bd50c21bb83b7c5baeaca65a0e4f
anjusrafael <anjusrafael@gmail.com>
2024-03-11 11:52:10 -0300

    Sua mensagem de commit aqui 56

--- remove_comments.sh
+++ remove_comments.sh
@@ -2,9 +2,12 @@
 
 # Define uma função para remover comentários de um arquivo
 remove_comments() {
+    echo "Removendo comentários de $1"
     sed '/^\s*#/d; s/#.*//' $1 > $1.tmp
     mv $1.tmp $1
+    echo "Comentários removidos de $1"
 }
+
 
 # Percorre recursivamente os arquivos .php no diretório do projeto
 find . -type f -name "*.php" | while read file; do
