e0e56bb0f04dbd143b9d925482f13256eab9b8d4
anjusrafael <anjusrafael@gmail.com>
2024-03-11 20:25:30 -0300

    oi 1

--- teste.php
+++ teste.php
@@ -1,10 +1,7 @@
-# Crie um arquivo de teste com alguns comentários
 echo "// Comentário 1" > teste.php
-echo "# Comentário 2" >> teste.php
+echo "
 echo "Código" >> teste.php
 
-# Execute a função remove_comments no arquivo de teste
 remove_comments "teste.php"
 
-# Verifique se os comentários foram removidos corretamente
 cat teste.php
